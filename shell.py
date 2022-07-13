b9245627a40505d971e71d543894302c7a0f59cd
srcrep <106826286+srcrep@users.noreply.github.com>
2022-07-13 13:51:45 -0500

    Update shell.py

--- shell.py
+++ shell.py
@@ -1,10 +1,10 @@
-import input
+from userin import *
 
-P = input.P
-R = input.R
-S = input.S
-E = input.E
-CA = input.CA
+P = userin.P
+R = userin.R
+S = userin.S
+E = userin.E
+CA = userin.CA
 
 name = "Cylindrical Shell"
 
@@ -13,5 +13,3 @@ ug27a = "= (P*R)/((S*E)-(0.6*P)) per UG-27 (c)(1)"
 printug27a = ("= (",P,"*",R,")/((",S,"*",E,")-(0.6*",P,"))")
 t = (P*R)/((S*E)-(0.6*P)) 
 tCA = (P*R)/((S*E)-(0.6*P)) + CA
-
-
