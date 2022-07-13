7fb8d7427fcfe0c07aff15fc2290a812f37bb680
srcrep <106826286+srcrep@users.noreply.github.com>
2022-07-13 13:58:45 -0500

    Update userin.py

--- userin.py
+++ userin.py
@@ -1,1 +1,6 @@
-
+P = input(float("Design Pressure : "))
+D = input(float("Inside Diameter : "))
+S = input(float("Material Stress : "))
+E = input(float("Joint Efficiency : "))
+CA = input(float("Corrosion Allowance : "))
+R = D/2
